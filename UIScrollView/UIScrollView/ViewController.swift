4c8e2cba46f81bfda6de76533168ef6db929b891
tunv <>
2020-01-16 14:42:14 +0700

    update slide!!

--- UIScrollView/UIScrollView/ViewController.swift
+++ UIScrollView/UIScrollView/ViewController.swift
@@ -9,12 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var svl_Main: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        svl_Main.delegate = self
     }
+    
+    @IBAction func scrollTo(_ sender: Any) {
+        svl_Main.contentInset = UIEdgeInsets.init(top: 100, left: 100, bottom: 300, right: 100)
+    }
+}
 
-
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
+    }
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        
+    }
 }
 
