473dada636dca73c38ec8eadf68d1fef6a3b3187
tunv <>
2020-02-10 09:52:10 +0700

    update slide rest api!!

--- RESTAPI/RESTAPI/ViewController.swift
+++ RESTAPI/RESTAPI/ViewController.swift
@@ -82,28 +82,17 @@ class ViewController: UIViewController {
         let url = String(format: "http://dummy.restapiexample.com/api/v1/employees")
         guard let serviceUrl = URL(string: url) else { return }
         let request = URLRequest(url: serviceUrl)
-        let session = URLSession.shared
         
+        let session = URLSession.shared
         session.dataTask(with: request) { (data, response, error) in
             print(data?.toString())
             //update UI => callback
-            //            if let response = response {
-            //                print(response)
-            //            }
-            //            if let data = data {
-            //                do {
-            //                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-            //                    print(json)
-            //                } catch {
-            //                    print(error)
-            //                }
-            //            }
         }.resume()
     }
     
     func addEmployee() {
-        let url = String(format: "http://dummy.restapiexample.com/api/v1/create")
-        guard let serviceUrl = URL(string: url) else { return }
+        let urlString = String(format: "http://dummy.restapiexample.com/api/v1/create")
+        guard let serviceUrl = URL(string: urlString) else { return }
         let parameterDictionary = ["name":"test12311","salary":"123","age":"23"]
         var request = URLRequest(url: serviceUrl)
         request.httpMethod = "POST"
